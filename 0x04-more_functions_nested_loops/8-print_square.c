f7e2fdafa59d63f635a904c4e64e3a02101c41c4
ayoubjlassi <ayoubjlassi818@gmail.com>
2023-07-20 11:54:45 -0700

    ab

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description:  prints _putchar, followed by a new line.
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int row, colum;
+
+	for (row 1; row <= size; row++)
+	{
+		for (colum = 1; colum <= size; colum++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
