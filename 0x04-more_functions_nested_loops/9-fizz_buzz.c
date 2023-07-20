ba9409aa89108bffdd8cdf0ce05e59f18430857b
ayoubjlassi <ayoubjlassi818@gmail.com>
2023-07-20 13:23:46 -0700

    ab

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,7 +16,7 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && !(num % == 0))
+		if (num % 3 == 0 && !(num % 5 == 0))
 			printf("Fizz");
 		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
@@ -24,6 +24,7 @@ int main(void)
 			printf("FizzBuzz");
 		else
 			printf("%d", num);
+
 		if (num != 100)
 			printf(" ");
 		else
