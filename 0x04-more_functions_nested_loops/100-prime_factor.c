fd715b6362d4a2853ffb3bf65d61ad407c6cec91
ayoubjlassi <ayoubjlassi818@gmail.com>
2023-07-20 14:13:15 -0700

    ab

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,16 +12,16 @@ int main(void)
 	long int n;
 	long int max;
 	long int i;
-	
+
 	n = 612852475143;
 	max = -1;
-	
+
 	while (n % 2 == 0)
 	{
 		max = 2;
 		n /= 2;
 	}
-	
+
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
@@ -32,8 +32,8 @@ int main(void)
 	}
 	if (n > 2)
 		max = n;
-	
+
 	printf("%ld\n", max);
-	
+
 	return (0);
 }
