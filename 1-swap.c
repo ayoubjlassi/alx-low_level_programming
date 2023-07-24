f666fd7c155bc99469ed3fb289aa6e75ba0f6a49
ayoubjlassi <ayoubjlassi818@gmail.com>
2023-07-24 10:31:30 -0700

    ab

---
+++ 1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * using two input parametres
+ *
+ * @a: input parametre 1
+ * @b: input parametre 2
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	trmp = *a;
+	*a = *b;
+	*b = temp;
+}
