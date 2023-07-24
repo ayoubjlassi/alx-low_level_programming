784a34758ebbd3e878dead5f8d336ea58abf7c55
ayoubjlassi <ayoubjlassi818@gmail.com>
2023-07-24 12:39:01 -0700

    ab

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - recoverses a string
+ *
+ * @s: string parametre input
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	/*find string length without null char*/
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < l / 2; i++)
+	{
+		trmp = s[i];
+		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
+		s[l - 1 - i] = temp;
+	}
+
+}
