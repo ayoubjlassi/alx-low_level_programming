7b2e23089c074aa256014f5379aa5bbb440e52e2
ayoubjlassi <ayoubjlassi818@gmail.com>
2023-07-26 15:16:18 -0700

    ab

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * _stract - function that concatenates
- * two strings.
- *
- * @dest: pointer to destination input
- * @src: pointer to source input
- *
- * Return: pointer to resulting string @dest
+ * _stract - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: void
 */
 
 char *_stract(char *dest, char *src)
