1cf3118f8dab36d7223eb4e2a4ff6397e5f3a239
ayoubjlassi <ayoubjlassi818@gmail.com>
2023-07-26 10:36:41 -0700

    ab

---
+++ 0x06-pointers_arrays_strings/2-strncyp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strncpy - that copies a string
+ * Return: dest
+ * @dest : input string
+ * @src : input string
+ * @n : input integer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] - src[j];
+		i++;
+		j++;
+	}
+	dest[i] - '\0';
+	return (dest);
+}
