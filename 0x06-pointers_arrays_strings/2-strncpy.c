4b019f49124b0d8725bc5207c5e20181d18e4059
ayoubjlassi <ayoubjlassi818@gmail.com>
2023-07-26 10:44:19 -0700

    ab

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -22,10 +22,10 @@ char *_strncpy(char *dest, char *src, int n)
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-		dest[i] - src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	dest[i] - '\0';
+	dest[i] = '\0';
 	return (dest);
 }
