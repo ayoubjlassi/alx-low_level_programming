c86926ded6843fe5b8b1cbb815bad728c8f3e443
ayoubjlassi <ayoubjlassi818@gmail.com>
2023-08-14 07:21:33 -0700

    ab

---
+++ 101-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
+
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
