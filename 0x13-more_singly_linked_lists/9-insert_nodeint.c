5180df9d5af17d64418f95291156813496bd673e
ayoubjlassi <ayoubjlassi818@gmail.com>
2023-08-29 15:16:34 -0700

    done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,8 +28,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 
-
 	if (h == NULL && idx != 0)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
@@ -39,5 +42,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = *head;
 		*head = new;
 	}
+	else
+	{
+		new->next = h->next;
+		h->next = new;
+	}
+
 	return (new);
 }
